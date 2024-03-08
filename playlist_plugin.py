336f0e38cd58de97af3311a9abf37c5d547d8582
red <red@redttg.com>
2024-03-08 17:10:53 +0200

    edits

--- playlist_plugin.py
+++ playlist_plugin.py
@@ -1,8 +1,22 @@
-from download_plugin import download_youtube
+import threading
+
+from download_plugin import download_link
 from pytube import Playlist
 
 playlist = Playlist("https://music.youtube.com/playlist?list=PLDInnU04fQ9aMylzx_-oVJogshRvUOEya&si=aXFSdSk4-m7OgS7w")
 
-for i, video in enumerate(playlist.videos_generator()):
-    download_youtube(video)
-    print(f'[{i}] Downloaded "{video.title}"')
+threads = []
+
+THREADS_LIMIT = 20
+
+for link in playlist.url_generator():
+    while threading.active_count() > THREADS_LIMIT:
+        print("Waiting for threads to finish", end="\r")
+        pass
+
+    thread = threading.Thread(target=download_link, args=(link,))
+    thread.start()
+    threads.append(thread)
+
+    for thread in threads:
+        thread.join()
